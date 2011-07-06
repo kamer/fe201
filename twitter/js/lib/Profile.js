e7407013a4d32f17de02053903c33086b3e251e1
kamer guzel <zurquessa@gmail.com>
2011-07-06 02:13:33 -0700

    Created Profile class.

--- twitter/js/lib/Profile.js
+++ twitter/js/lib/Profile.js
@@ -1,4 +1,16 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kamer
+ * Date: 05/07/11
+ * Time: 12:13 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var Profile = function (bio, photoUrl, location, userId){
+    this.id = tart.getUId();
+    this.userId = userId;
+    this.bio = bio;
+    this.photoUrl = photoUrl;
+    this.location = location;
+
+};
