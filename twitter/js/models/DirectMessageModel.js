c107b742216691982631c83fd3190af7e29297ce
kamer guzel <zurquessa@gmail.com>
2011-07-06 02:18:24 -0700

    Implemented DirectMessageModel class with all its methods.

--- twitter/js/models/DirectMessageModel.js
+++ twitter/js/models/DirectMessageModel.js
@@ -1,12 +1,45 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kamer
+ * Date: 05/07/11
+ * Time: 1:17 AM
+ * To change this template use File | Settings | File Templates.
+ */
 
-var DirectMessageModel = function() {
+var DirectMessageModel = function(){
+    
+};
+
+DirectMessageModel.prototype.sendDirectMessage = function (from, to, body){
+    var directMessage = new DirectMessage(from, to, body);
+    localStorage.setObject(directMessage.id,directMessage);
 
 };
 
-DirectMessageModel.prototype.createDirectMessage = function(from, to, body){
-    
-}
+DirectMessageModel.prototype.getInboxByUserId = function (userid){
+     var arr = [];
+    //bunu direkt user model'dan da cagirabilirsin, simdilik dur
+     for (var directMessage in localStorage){
+        directMessage = localStorage.getObject(directMessage);
+        if (directMessage && directMessage.to.user.id && directMessage.to.user.id==userid){
+            arr.push(directMessage);
+            
+        }
+    }
+    return arr;
+
+};
+
+DirectMessageModel.prototype.getSentByUserId = function (userid){
+     var arr = [];
+        //bunu direkt user model'dan da cagirabilirsin, simdilik dur
+         for (var directMessage in LocalStorage){
+            directMessage = localStorage.getObject(directMessage);
+            if (directMessage && directMessage.from.user.id && directMessage.from.user.id==userid){
+                arr.push(directMessage);
+
+            }
+        }
+        return arr;
+
+};
