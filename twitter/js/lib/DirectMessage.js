84b33ea08d9b2ed2664ba22dfb223645bef3c366
kamer guzel <zurquessa@gmail.com>
2011-07-06 02:09:51 -0700

    Created DirectMessage class.

--- twitter/js/lib/DirectMessage.js
+++ twitter/js/lib/DirectMessage.js
@@ -1,4 +1,15 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kamer
+ * Date: 05/07/11
+ * Time: 12:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var DirectMessage = function(id, from, to, body){
+    this.id = tart.getUId();
+    this.from;
+    this.to;
+    this.body;
+
+};
