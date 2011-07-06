b4295b9c82cbb54fb8a593a79183dd7351a00ccb
kamer guzel <zurquessa@gmail.com>
2011-07-06 02:28:41 -0700

    [Follow] Modified FollowManager class: changed parameter names for createFollow function

--- twitter/js/models/FollowManager.js
+++ twitter/js/models/FollowManager.js
@@ -1,13 +1,41 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kamer
+ * Date: 05/07/11
+ * Time: 2:00 AM
+ * To change this template use File | Settings | File Templates.
+ */
 
-var FollowManager = function() {
+var FollowManager = function(){
 
 };
 
-FollowManager.prototype.createFollow = function(follower, following) {
-    var follow = new Follow(follower.id, following.id);
+FollowManager.prototype.createFollow = function(followerId, followingId){
+    var follow = new Follow (followerId, followingId);
     localStorage.setObject(follow.id, follow);
-}
+};
+
+FollowManager.prototype.getFollowingByUserId = function(userId){
+     var arr = [];
+     for (var following in localStorage){
+        following = localStorage.getObject(following);
+        if (following && following.userId && following.userId == userId){
+            arr.push(following);
+        }
+    }
+    return arr;
+
+};
+
+FollowManager.prototype.getFollowersByUserId = function(userId){
+    var arr = [];
+       for (var follower in localStorage){
+          follower = localStorage.getObject(follower);
+          if (follower && follower.userId && follower.userId == userId){
+              arr.push(follower);
+          }
+      }
+      return arr;
+
+};
+
