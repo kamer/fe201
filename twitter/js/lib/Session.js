9d6b195a4f699ed9fe6f113232d876a098a19d6f
kamer guzel <zurquessa@gmail.com>
2011-07-06 14:47:21 -0700

    [Session] Added google closure provide and require dependencies. Added "twitter." before class name.

--- twitter/js/lib/Session.js
+++ twitter/js/lib/Session.js
@@ -6,7 +6,20 @@
  * To change this template use File | Settings | File Templates.
  */
 
-var Session = function (userId) {
+/**
+ * @fileoverview Session class
+ */
+goog.provide('twitter.Session');
+goog.require('tart.getUId');
+
+
+/**
+ * Session class.
+ *
+ * @constructor
+ */
+
+twitter.Session = function (userId) {
     this.id = tart.getUId();
     this.userId=userId;
 };
