a565efbda90ff85cbf459f2e09a91c06a069406f
kamer guzel <zurquessa@gmail.com>
2011-07-06 02:14:29 -0700

    Created Session class for the twitter application.

--- twitter/js/lib/Session.js
+++ twitter/js/lib/Session.js
@@ -1,4 +1,12 @@
-// Copyright (c) 2009-2010 Techinox Information Technologies (http://www.techinox.com)
-// Techinox Commercial License
-//
-// @author Armagan Amcalar <armagan@tart.com.tr>
+/**
+ * Created by JetBrains PhpStorm.
+ * User: kamer
+ * Date: 05/07/11
+ * Time: 4:21 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+var Session = function (userId) {
+    this.id = tart.getUId();
+    this.userId=userId;
+};
