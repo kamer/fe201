59e9bdfdb425a20403bc5b6927350b1bca1fc67b
kamer <zurquessa@gmail.com>
2011-07-19 03:29:08 -0700

    No actual changes.

--- twitter/js/models/ProfileModel.js
+++ twitter/js/models/ProfileModel.js
@@ -6,12 +6,14 @@
  * To change this template use File | Settings | File Templates.
  */
 
+
 /**
  * @fileoverview Profile Model class that deals with users' profile
  */
 
 goog.provide('twitter.ProfileModel');
 goog.require('twitter.Profile');
+goog.require('tart.storage.Storage');
 
 /**
  * Profile Model class. Has utility functions that get and manipulate user data
@@ -24,14 +26,14 @@ twitter.ProfileModel = function(){
 };
 
 twitter.ProfileModel.prototype.createProfile = function(bio, photoUrl, location){
-    var profile = new Profile(bio, photoUrl, location);
-    localStorage.setObject(profile.id, profile);
+    var profile = new twitter.Profile(bio, photoUrl, location);
+    twitter.localStorage.set(profile.id, profile);
 
 };
 
 twitter.ProfileModel.prototype.getProfileByUserId = function(userId){
      for (var profile in localStorage){
-        profile = localStorage.getObject(profile);
+        profile = twitter.localStorage.get(profile);
         if (profile && profile.userId && profile.userId == userId){
             return profile;
         }
@@ -43,12 +45,12 @@ twitter.ProfileModel.prototype.getProfileByUserId = function(userId){
 twitter.ProfileModel.prototype.editProfileByUserId = function(userId, bio, photoUrl, location){
     
     for (var profile in localStorage){
-        profile = localStorage.getObject(profile);
+        profile = twitter.localStorage.get(profile);
         if (profile && profile.userId && profile.userId == userId){
             profile.bio = bio;
             profile.photoUrl = photoUrl;
             profile.location = location;
-            localStorage.setObject(profile.id, profile);
+            twitter.localStorage.set(profile.id, profile);
         }
     }
     return false;
