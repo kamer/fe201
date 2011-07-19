2451f78fd5ffeac4884b0cf710d0213206cdc94c
kamer <zurquessa@gmail.com>
2011-07-19 08:28:37 -0700

    Changed google provide dependency of twitter to twitter.Application. Started wrtiting a initRouting function.

--- twitter/js/twitter/application/twitter.js
+++ twitter/js/twitter/application/twitter.js
@@ -10,7 +10,8 @@ goog.require('tart.mvc');
 goog.require('twitter.controllers.SetupController');
 goog.require('twitter.controllers.IndexController');
 goog.require('tart.storage.Storage');
-goog.provide('twitter');
+//goog.provide('twitter');
+goog.provide('twitter.Application');
 
 
 
@@ -19,9 +20,23 @@ goog.provide('twitter');
  * @extends {tart.mvc.Application}
  */
 twitter.Application = function() {
+
     goog.base(this);
     
 };
 
 goog.inherits(twitter.Application, tart.mvc.Application);
 
+/**
+ * @inheritDoc
+ */
+twitter.Application.prototype.initRouting = function() {
+    var router = this.getRouter();
+    router.addRoute(new tart.mvc.uri.Route({
+        name: 'homeIndex',
+        format: 'home/index/',
+        controller: twitter.controllers.IndexController,
+      //  action: twitter.controllers.IndexController.indexAction
+    }));
+
+};
